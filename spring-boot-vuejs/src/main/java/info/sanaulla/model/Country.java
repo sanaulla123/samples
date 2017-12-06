55b9c2b6da5e205cfcd6d7ec605ec85648dfa599
Mohamed Sanaulla <sanaulla123@gmail.com>
2017-12-07 00:52:28 +0300

    vuejs component sample

--- spring-boot-vuejs/src/main/java/info/sanaulla/model/Country.java
+++ spring-boot-vuejs/src/main/java/info/sanaulla/model/Country.java
@@ -1,5 +1,6 @@
 package info.sanaulla.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
@@ -22,6 +23,24 @@ public class Country {
 	private String name;
 	private String continent;
 	private String region;
+	
+	private Double surfacearea;
+	/*
+	 *  indepyear smallint,
+    population integer NOT NULL,
+    lifeexpectancy real,
+    gnp numeric(10,2),
+    gnpold numeric(10,2),
+    localname text NOT NULL,
+    governmentform text NOT NULL,
+    headofstate text,
+    capital integer,
+	 */
+	private Integer indepyear;
+	private Integer population;
+	
+	@Column("lifeexpectancy")
+	private Double lifeExpectancy;
 	
 	@OneToOne()
 	@JoinColumn(name = "capital" )
